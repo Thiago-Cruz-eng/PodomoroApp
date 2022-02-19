9770f305c34e3374abc14a50a1563c191742aaf9
Thiago Cruz <morim24093@gmail.com>
2022-02-19 10:43:01 -0300

    Start with setInterval and same Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { PomodoroTimer } from './components/pomodoro-tomer';
 
 function App() {
-  return <div className="App">ola mundo!</div>;
+  return (
+    <div className="App">
+      <PomodoroTimer defaultPomodoroTimer={3600} />
+    </div>
+  );
 }
 
 export default App;
